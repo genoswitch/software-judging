65547ca2d807a3199833e3c625764f24e83ba84e
James <jcahill773@gmail.com>
2023-01-28 09:46:50 +0000

    Add comments, move prototypes to .h file

--- src/main.c
+++ src/main.c
@@ -1,33 +1,42 @@
+// FreeRTOS
 #include "FreeRTOS.h"
 
+// Standard libraries
+#include <stdio.h>
+#include "pico/stdlib.h"
 
-static void prvSetupHardware( void );
+// Accompanying header file
+#include "main.h"
 
-int main( void )
+int main(void)
 {
+    // Setup hardware (init stdio, etc.)
     prvSetupHardware();
+
+    // Determine which RTOS type we are running
     const char *rtos_name;
-#if ( portSUPPORT_SMP == 1 )
+#if (portSUPPORT_SMP == 1)
     rtos_name = "FreeRTOS SMP";
 #else
     rtos_name = "FreeRTOS";
 #endif
 
-#if ( portSUPPORT_SMP == 1 ) && ( configNUM_CORES == 2 )
+#if (portSUPPORT_SMP == 1) && (configNUM_CORES == 2)
     printf("%s on both cores:\n", rtos_name);
 #endif
 
-#if ( mainRUN_ON_CORE == 1 )
+#if (mainRUN_ON_CORE == 1)
     printf("%s on core 1:\n", rtos_name);
 #else
     printf("%s on core 0:\n", rtos_name);
 #endif
 
+    // Init some tasks!
+
     return 0;
 }
 
-
-static void prvSetupHardware( void )
+static void prvSetupHardware(void)
 {
     stdio_init_all(); // init the things uwu
 }
