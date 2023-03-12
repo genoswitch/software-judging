d27c351e42d3072d798c6ef11ca531b8b36699a3
James <jcahill773@gmail.com>
2023-03-12 10:48:01 +0000

    (bulk): Create base_message struct, send queue contents over usb

--- src/tasks/bulk.c
+++ src/tasks/bulk.c
@@ -1,31 +1,36 @@
-// FreeRTOS core and task libraries
+// FreeRTOS core, task and queue libraries
 #include "FreeRTOS.h"
 #include "task.h"
 #include "queue.h"
 
+// TinyUSB
+#include "tusb.h"
+
+// Pico stdio and standard lib
 #include <stdio.h>
 #include "pico/stdlib.h"
 
+// Accompanying header file
 #include "bulk.h"
 
-#define MCU_TEMP_STACK_SIZE configMINIMAL_STACK_SIZE
+/* Declare a variable of type QueueHandle_t to hold the handle of the queue being created. */
+QueueHandle_t xPointerQueue;
+
+#define MCU_BULK_QUEUE_STACK_SIZE configMINIMAL_STACK_SIZE
 
 #if configSUPPORT_STATIC_ALLOCATION
-StackType_t mcu_temp_stack[MCU_TEMP_STACK_SIZE];
+StackType_t mcu_temp_stack[MCU_BULK_QUEUE_STACK_SIZE];
 StaticTask_t mcu_temp_taskdef;
 #endif
 
 void pvRegisterBulkQueueTask(void)
 {
 #if configSUPPORT_STATIC_ALLOCATION
-    xTaskCreateStatic(bulk_queue_task, "bulk", MCU_TEMP_STACK_SIZE, NULL, configMAX_PRIORITIES, mcu_temp_stack, &mcu_temp_taskdef)
+    xTaskCreateStatic(bulk_queue_task, "bulkqd", MCU_BULK_QUEUE_STACK_SIZE, NULL, configMAX_PRIORITIES, mcu_temp_stack, &mcu_temp_taskdef)
 #else
-    xTaskCreate(bulk_queue_task, "bulk", MCU_TEMP_STACK_SIZE, NULL, configMAX_PRIORITIES, NULL);
+    xTaskCreate(bulk_queue_task, "bulkqd", MCU_BULK_QUEUE_STACK_SIZE, NULL, configMAX_PRIORITIES, NULL);
 #endif
 }
-
-/* Declare a variable of type QueueHandle_t to hold the handle of the queue being created. */
-QueueHandle_t xPointerQueue;
 
 QueueHandle_t getQueueHandle(void)
 {
@@ -38,9 +43,9 @@ void pvCreateBulkQueue(void)
     /* Create a queue that can hold a maximum of 5 pointers, in this case character pointers. */
     xPointerQueue = xQueueCreate(
         // The number of items the queue can hold
-        5,
+        10,
         // Size of each item only big enough for a pointer
-        sizeof(&xMessage));
+        sizeof(&base_message));
 
     if (xPointerQueue == NULL) // 0 == NULL
     {
@@ -59,7 +64,7 @@ void bulk_queue_task(void *param)
     (void)param; // prevent unused var warning
 
     // Create a char to store the pointer from the buffer object in the queue
-    struct AMessage xRecievedStruct, *pxRecievedPointer;
+    struct BaseMessage xRecievedStruct, *pxRecievedPointer;
 
     while (1)
     {
@@ -67,7 +72,8 @@ void bulk_queue_task(void *param)
         if (xQueueReceive(xPointerQueue, &(pxRecievedPointer), portMAX_DELAY) == pdTRUE)
         {
             // We have data and have recieved it successfully
-            printf("uwu");
+            tud_vendor_write(pxRecievedPointer, pxRecievedPointer->bLength);
+            tud_vendor_flush();
         }
         else
         {
--- src/tasks/bulk.h
+++ src/tasks/bulk.h
@@ -1,14 +1,16 @@
+// FreeRTOS core and queue libraries for QueueHandle_t typedef
+#include "FreeRTOS.h"
+#include "queue.h"
+
 void pvCreateBulkQueue(void);
 void bulk_queue_task(void *param);
-void pvRegisterBulkQueueTask(void);
-
-#include "FreeRTOS.h"
-#include "task.h"
-#include "queue.h"
 QueueHandle_t getQueueHandle(void);
 
-struct AMessage
+#define MAGIC_BYTE 0xdd24433b // Bear in mind this is little endian so will show up backwards!
+
+struct BaseMessage
 {
-    char ucMessageID;
-    char ucData[20];
-} xMessage;
+    uint32_t bMagic;
+    uint8_t bLength;
+    uint8_t bCode;
+} base_message;
