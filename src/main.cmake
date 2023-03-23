638e77ab2100070def140eaaa59c5dfe57f0b317
James <jcahill773@gmail.com>
2023-03-23 11:40:24 +0000

    (cmake/main): Create subproject name var

--- src/main.cmake
+++ src/main.cmake
@@ -2,6 +2,8 @@ set(PICO_SDK_PATH ../lib/pico-sdk)
 set(FREERTOS_KERNEL_PATH ../lib/freertos-smp)
 set(PICO_TINYUSB_PATH ${CMAKE_CURRENT_LIST_DIR}/../lib/tinyusb)
 
+# 'Subproject' name
+set (MAIN main)
 
 # Pull in SDK (must be before project)
 include(pico_sdk_import.cmake)
@@ -18,17 +20,17 @@ pico_sdk_init()
 
 
 # Add executable targets
-add_executable(main main.c)
+add_executable(${MAIN} main.c)
 
 
 # RTOS config "FreeRTOSConfig.h"
-target_include_directories(main PRIVATE 
+target_include_directories(${MAIN} PRIVATE 
     ${CMAKE_CURRENT_LIST_DIR}/rtos-config/
     ${CMAKE_CURRENT_LIST_DIR}/tinyusb-config/
 
 )
 
-target_sources(main PUBLIC
+target_sources(${MAIN} PUBLIC
     ${CMAKE_CURRENT_LIST_DIR}/main.c
     ${CMAKE_CURRENT_LIST_DIR}/usb/tasks.c
     ${CMAKE_CURRENT_LIST_DIR}/usb/vendor_request.c
@@ -44,16 +46,16 @@ target_sources(main PUBLIC
 # pico_stdlib needed as FreeRTOS uses panic_unsupported
 # memory management: FreeRTOS-Kernel-Heap# required for pvPortMalloc
 # tinyusb_device tinyusb_board (https://github.com/raspberrypi/pico-examples/blob/master/usb/device/dev_hid_composite/CMakeLists.txt)
-target_link_libraries(main pico_stdlib hardware_adc pico_unique_id FreeRTOS-Kernel FreeRTOS-Kernel-Heap4 tinyusb_device tinyusb_board cmake_git_version_tracking)
+target_link_libraries(${MAIN} pico_stdlib hardware_adc pico_unique_id FreeRTOS-Kernel FreeRTOS-Kernel-Heap4 tinyusb_device tinyusb_board cmake_git_version_tracking)
 
 # stdio only on UART (UART0 by default, pins 1 and 2)
-pico_enable_stdio_usb(main 0)
-pico_enable_stdio_uart(main 1)
+pico_enable_stdio_usb(${MAIN} 0)
+pico_enable_stdio_uart(${MAIN} 1)
 
 # Create extra files, such as .uf2
-pico_add_extra_outputs(main)
+pico_add_extra_outputs(${MAIN})
 
 # Based on lib/pico-flashloader/CMakeLists.txt#L86
 # Use a separate linker script for the application to make sure it is built
 # to run at the right location (after the flashloader).
-set_linker_script(main memmap_application.ld)
+set_linker_script(${MAIN} memmap_application.ld)
