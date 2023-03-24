f1f96578272d21d46b1a1449707e927f8fa7a511
James <jcahill773@gmail.com>
2023-03-24 12:41:50 +0000

    (cmake): Update linker helper for new linker files

---
+++ src/linker/helper.cmake
@@ -0,0 +1,10 @@
+# Submodule path
+set (LINKER_FOLDER ${CMAKE_CURRENT_LIST_DIR})
+
+# Helper function (based on lib/pico-flashloader/CMakeLists.txt#L22)
+function(set_linker_script TARGET script)
+    target_link_directories(${TARGET} PRIVATE ${LINKER_FOLDER})
+    # Add dependencies on the 'included' linker scripts so that the target gets
+    # rebuilt if they are changed
+    pico_set_linker_script(${TARGET} ${LINKER_FOLDER}/${script})
+endfunction()
