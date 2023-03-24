d026a2810762b2a39d4fd106ee2c2a780f0098d2
James <jcahill773@gmail.com>
2023-03-24 12:46:20 +0000

    (cmake): PoC new way of combining binaries
    This method is based off of picowota (https://github.com/usedbytes/picowota) as well as the pico sdk itself.
    
    With this method we can create all of the expected output files (.elf, .hex, .uf2) as well as register it as an executable in cmake.
    
    This commit is just a proof of concept, in later commits this will be extended to have the functionality described above.

--- src/combined.cmake
+++ src/combined.cmake
@@ -1,19 +1,18 @@
-# Based on lib/pico-flashloader/CMakeLists.txt#L110
+# Combine the flashloader and main binaries to one bootable UF2.
 
-# Combine the flashloader and main application into one flashable image!
+set(APP_BIN ${CMAKE_CURRENT_BINARY_DIR}/main.bin)
+set(COMBINED combined_new)
+add_custom_target(${COMBINED} ALL)
+add_dependencies(${COMBINED} main pico-flashloader)
+add_custom_command(TARGET ${COMBINED} DEPENDS ${APP_BIN}
+	COMMAND ${CMAKE_OBJCOPY}
+		--update-section .app=${APP_BIN} ${CMAKE_CURRENT_BINARY_DIR}/pico-flashloader ${COMBINED}.elf
+)
 
-set(COMPLETE_UF2 ${CMAKE_CURRENT_BINARY_DIR}/main_full.uf2)
+add_custom_command(TARGET ${COMBINED} POST_BUILD
+    COMMAND ${CMAKE_OBJCOPY} -Obinary ${COMBINED}.elf ${COMBINED}.bin
+)
 
-find_package (Python3 REQUIRED COMPONENTS Interpreter)
-add_custom_command(OUTPUT ${COMPLETE_UF2} DEPENDS ${FLASHLOADER} ${MAIN}
-        COMMENT "Building combined UF2 image (${FLASHLOADER} + ${MAIN})"
-        COMMAND ${Python3_EXECUTABLE}
-                ${FLASHLOADER_MODULE_PATH}/uf2tool.py
-                -o ${COMPLETE_UF2} ${FLASHLOADER_UF2} ${MAIN_UF2}
-        )
-
-# Make the entire project depend on the combine stage
-add_custom_target(${PROJECT} ALL DEPENDS ${COMPLETE_UF2})
-
-# Does not seem to be needed?
-# install(FILES ${COMPLETE_UF2} DESTINATION ${CMAKE_INSTALL_PREFIX} )
+add_custom_command(TARGET ${COMBINED} POST_BUILD 
+    COMMAND ${CMAKE_OBJCOPY} -Oihex  ${COMBINED}.elf ${COMBINED}.hex
+)
