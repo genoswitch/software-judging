bd4b049d3725b3e544746bc97cf47e00cbcebbf3
James <jcahill773@gmail.com>
2023-04-06 09:54:55 +0100

    (cmake/combined): Generate a combined uf2

--- src/combined.cmake
+++ src/combined.cmake
@@ -18,3 +18,27 @@ add_custom_command(TARGET ${COMBINED} POST_BUILD
 add_custom_command(TARGET ${COMBINED} POST_BUILD 
     COMMAND ${CMAKE_OBJCOPY} -Oihex  ${COMBINED}.elf ${COMBINED}.hex
 )
+
+# Create a UF2.
+# pico-flashloader uses custom python script to both combine and create the final uf2.
+# However, we now use another method using objcopy similar to what picowota does.
+# <https://github.com/usedbytes/picowota>
+# Usedbytes demonstrates that the pico sdk has a tool for creating uf2 files.
+# We can simply call the same tool to generate our combined uf2.
+
+# 1. Find ELF2UF2 if it is not already available/found
+if (NOT ELFUF2_FOUND)
+    # Append the Pico SDK tools directory to the cmake PATH.
+    set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${PICO_SDK_PATH}/tools)
+    # Call CMake's find_package function.
+    find_package(ELF2UF2)
+endif()
+
+# 2. ELF2UF2 should now be available.
+if (ELF2UF2_FOUND)
+    add_custom_command(TARGET ${COMBINED} POST_BUILD
+        COMMAND ELF2UF2 ${COMBINED}.elf ${COMBINED}.uf2
+    )
+else()
+    message(FATAL_ERROR "ELF2UF2 not found in the pico sdk. Unable to generate ${COMBINED}.uf2")
+endif()
