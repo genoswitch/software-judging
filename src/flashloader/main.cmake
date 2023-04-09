f0f2cf7ee439a88370811c064f6d2203f0ab8014
James <jcahill773@gmail.com>
2023-04-09 10:13:32 +0100

    Add INCLUDES_FLASHLOADER preprocessor macro

--- src/flashloader/main.cmake
+++ src/flashloader/main.cmake
@@ -1,7 +1,11 @@
 set(MAIN_FL_LINKED intermediate_main_flashloader_linked)
 
 add_executable(${MAIN_FL_LINKED} core/main.c)
+
 target_link_libraries(${MAIN_FL_LINKED} ${MAIN})
+
+target_compile_definitions(${MAIN_FL_LINKED} PRIVATE INCLUDES_FLASHLOADER)
+
 # Based on lib/pico-flashloader/CMakeLists.txt#L86
 # Use a separate linker script for the application to make sure it is built
 # to run at the right location (after the flashloader).
