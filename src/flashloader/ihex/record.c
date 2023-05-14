ab0c20ac6b49d89a35cb32207d05929da60be7fb
James <jcahill773@gmail.com>
2023-05-14 16:40:44 +0100

    Add initial ihex parsing code (based on rhulme/pico-flashloader)

---
+++ src/flashloader/ihex/record.c
@@ -0,0 +1,46 @@
+#include "record.h"
+
+#include "util.c"
+
+// memset, memcpy
+#include "pico/mem_ops.h"
+
+// Converts an Intel hex record in text form to a binary representation.
+// Returns non-zero if the text could be parsed successfully
+int processRecord(const char *line, ihexRecord *record)
+{
+    int success = 0;
+    int offset = 0;
+    uint8_t value;
+    uint8_t data[256 + 5]; // Max payload 256 bytes plus 5 for fields
+    uint8_t checksum = 0;
+
+    // again, "" no work
+    while (*line && (*line != ':'))
+        line++;
+
+    if (*line++ == ':')
+    {
+        while (parseHex(line, &value) && (offset < sizeof(data)))
+        {
+            data[offset++] = value;
+            checksum += value;
+            line += 2;
+        }
+    }
+
+    // Check the checksum for a record (line)
+    // The checksum is a two's complement sum of the previous bytes in the record.
+    // https://en.wikipedia.org/wiki/Intel_HEX#:~:text=%C2%A0Checksum-,Checksum%20calculation,-%5Bedit%5D
+    // (rhulme): Final checksum should be 0 if everything is okay.
+    if ((offset > 0) && (checksum == 0))
+    {
+        record->count = data[0];
+        record->addr = data[2] | (data[1] << 8);
+        record->type = data[3];
+        memcpy(record->data, &data[4], data[0]);
+        success = 1;
+    }
+
+    return success;
+}
---
+++ src/flashloader/ihex/record.h
@@ -0,0 +1,21 @@
+#include <stdint.h>
+
+enum
+{
+    IHEX_TYPE_DATA,
+    IHEX_TYPE_EOF,
+    IHEX_TYPE_END_SEG_ADDR,
+    IHEX_TYPE_START_SEG_ADDR,
+    IHEX_TYPE_EXT_LIN_ADDR,
+    IHEX_TYPE_START_LIN_ADDR
+};
+
+typedef struct
+{
+    uint8_t count;
+    uint16_t addr;
+    uint8_t type;
+    uint8_t data[256]; // rhulme (max size 256bytes)
+} ihexRecord;
+
+int processRecord(const char *line, ihexRecord *record);
