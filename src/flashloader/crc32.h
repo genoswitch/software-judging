9d21c85745e70bb3d19059a24c9b9df89cebfd8d
James <jcahill773@gmail.com>
2023-06-08 15:51:58 +0100

    (flashloader/crc32) Add header file

---
+++ src/flashloader/crc32.h
@@ -0,0 +1,10 @@
+#ifndef FLASHLOADER_CRC32_H_
+#define FLASHLOADER_CRC32_H_
+
+#include <stdint.h>
+
+uint32_t crc32(void *ptr, uint32_t len);
+
+uint32_t crc32_no_reflect_xor(void *ptr, uint32_t len);
+
+#endif /* FLASHLOADER_CRC32_H_ */
