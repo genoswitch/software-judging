39b9163cdb4a6b3f9e40a6902107ee5d2ebf595c
James <jcahill773@gmail.com>
2023-06-09 14:19:13 +0100

    (flashloader/ihex/flash): Update header guards to reflect new file location

--- src/flashloader/ihex/flash.h
+++ src/flashloader/ihex/flash.h
@@ -1,6 +1,6 @@
-#ifndef FLASHLOADER_FLASH_H_
-#define FLASHLOADER_FLASH_H_
+#ifndef IHEX_FLASH_H_
+#define IHEX_FLASH_H_
 
-void flashImage(tFlashHeader* header, uint32_t length);
+void flashImage(tFlashHeader *header, uint32_t length);
 
-#endif /* FLASHLOADER_FLASH_H_ */
+#endif /* IHEX_FLASH_H_ */
