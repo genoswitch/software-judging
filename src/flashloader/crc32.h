328e6524f77aec6b08efea0a8578731d34e1e875
James <jcahill773@gmail.com>
2023-06-09 13:28:29 +0100

    (flashloader/crc32): Reduce code dupe, fix alternate crc32 calc for flashloader

--- src/flashloader/crc32.h
+++ src/flashloader/crc32.h
@@ -5,6 +5,6 @@
 
 uint32_t crc32(void *ptr, uint32_t len);
 
-uint32_t crc32_no_reflect_xor(void *ptr, uint32_t len);
+uint32_t crc32_no_reflection_or_final_xor(void *ptr, uint32_t len);
 
 #endif /* FLASHLOADER_CRC32_H_ */
