cd3865d0ac34f3d1b9aad7ada7460b40add41186
James Cahill <jcahill773@gmail.com>
2023-06-14 11:01:01 +0100

    (squash): Initial merging CI action
    commit 4011342ece50584bfff10b96bbbcc4b7fdb8c4a1
    Author: James Cahill <jcahill773@gmail.com>
    Date:   Wed Jun 14 10:59:29 2023 +0100
    
        (ci/merge): Cross reference commit on GitHub
    
    commit 796af9a05444234b099d44ea476214dba0fccf6f
    Author: James Cahill <jcahill773@gmail.com>
    Date:   Wed Jun 14 10:56:56 2023 +0100
    
        Partial revert "(ci/merge): Adjust speech marks"
    
        This reverts commit 467d9a1f7a6aeed1c85db30c7dea051eb7443ada. merge.yml only
    
    commit de8daa9e5c423f6bec5353007789a07d1b0a947f
    Author: James Cahill <jcahill773@gmail.com>
    Date:   Wed Jun 14 10:46:52 2023 +0100
    
        (ci/merge): Add -i tag to git commit
    
    commit 8df2dce9886fb7b954c7039f8682829b14cf9592
    Author: James Cahill <jcahill773@gmail.com>
    Date:   Wed Jun 14 10:45:04 2023 +0100
    
        (ci/merge): Adjust commit message
    
    commit 467d9a1f7a6aeed1c85db30c7dea051eb7443ada
    Author: James Cahill <jcahill773@gmail.com>
    Date:   Wed Jun 14 10:39:47 2023 +0100
    
        (ci/merge): Adjust speech marks
    
    commit 52b9c53fcfcb512415224558badf276243c96c49
    Author: James Cahill <jcahill773@gmail.com>
    Date:   Wed Jun 14 10:36:06 2023 +0100
    
        (ci/merge): Adjust name, add write permission
    
    commit 969f5167c8007811de79cfc329da2d97c4e8dad7
    Author: James Cahill <jcahill773@gmail.com>
    Date:   Wed Jun 14 10:32:40 2023 +0100
    
        (ci/merge): Set git committer information
    
    commit 4da6eb34c74c4c12ac5f45d34c95b14afd2c48e7
    Author: James Cahill <jcahill773@gmail.com>
    Date:   Wed Jun 14 10:24:54 2023 +0100
    
        (ci/merge): Use shell script to construct commit message
    
    commit 85e49c030a6cf0b7b168ae3286f8ada728d5ba8f
    Author: James Cahill <jcahill773@gmail.com>
    Date:   Wed Jun 14 10:20:39 2023 +0100
    
        (ci/merge): Use gh syntax to set env variable
    
    commit 90cc7c9e2afe81dbfad52e1b189d995832566a20
    Author: James Cahill <jcahill773@gmail.com>
    Date:   Wed Jun 14 10:18:14 2023 +0100
    
        (ci/merge): Use env variable for commit message
    
    commit ca0e97d9da120fcfc2c679947ac30937fe9d9cbc
    Author: James Cahill <jcahill773@gmail.com>
    Date:   Wed Jun 14 10:16:43 2023 +0100
    
        (ci): Initial merge ci action

---
+++ .github/workflows/commit-message-env.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SRC_LATEST_COMMIT=$(git log -n 1 --pretty=format:"%H" source/$SRC_BRANCH)
+
+ENV_VALUE="(merge/$SRC_REPO): Merge changes from $SRC_BRANCH (genoswitch/$SRC_REPO@$SRC_LATEST_COMMIT)"
+
+echo "ACTION_COMMIT_MSG="$ENV_VALUE"" >> $GITHUB_ENV
