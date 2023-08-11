a1b14c339e882256c70bce39fd7ae43f4a93ef15
Rishabh <r.das@cityoflondonschool.org.uk>
2023-08-11 13:09:31 -0400

    added stuff

--- app.py
+++ app.py
@@ -1,78 +1,93 @@
 import kivy
 kivy.require('2.2.1') # replace with your current kivy version !
 
+import thsGen
 from kivy.app import App
 from kivy.uix.label import Label
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.image import Image
-from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
+from kivy.core.window import Window
 
-class mainScreen(Screen):
-    pass
+kv = Builder.load_file("thsGenerator.kv")
 
-class resultScreen(Screen):
-    pass
+class mainWindow(Screen):
 
-class screenManager(ScreenManager):
-    pass
+    def addMiRNA(self):
+        print("miRNA added")
+        self.ids.inputGrid.add_widget(Label(text='miRNA name'))
+        self.ids.inputGrid.add_widget(TextInput(multiline=False))
 
-class mainPage(GridLayout):
-    def __init__(self, **kwargs):
-        super(mainPage, self).__init__(**kwargs)
-        
-        self.cols = 1
-
-        self.inputLayout = GridLayout()
-        self.inputLayout.cols = 4
-
-        self.add_widget(Image(source='GENOSWITCH_logo_Coloured.png'))
-
-        self.addMiRNAButton = Button(text = 'add miRNA')
-        self.addMiRNAButton.bind(on_press = self.addMiRNA)
-        self.add_widget(self.addMiRNAButton)
-
-        self.inputLayout.add_widget(Label(text='miRNA name'))
-        self.inputLayout.add_widget(TextInput(multiline=False))
-
-        self.inputLayout.add_widget(Label(text='miRNA sequence'))
-        self.inputLayout.add_widget(TextInput(multiline=False))
-
-        self.add_widget(self.inputLayout)
-
-        self.submitButton = Button(text='submit')
-        self.submitButton.bind(on_press = self.submit)
-        self.add_widget(self.submitButton)
-
-    def addMiRNA(self, instance):
-        self.inputLayout.add_widget(Label(text='miRNA name'))
-        self.inputLayout.add_widget(TextInput(multiline=False))
-
-        self.inputLayout.add_widget(Label(text='miRNA sequence'))
-        self.inputLayout.add_widget(TextInput(multiline=False))
+        self.ids.inputGrid.add_widget(Label(text='miRNA sequence'))
+        self.ids.inputGrid.add_widget(TextInput(multiline=False))
     
-    def submit(self, instance):
+    def submit(self):
+        print("submitted")
+        # print(self.children[0])
+        # print(self.children[0].children)
+        # print(self.children[0].children[2].children)
+
         contents = []
-        for i, child in enumerate(self.inputLayout.children):
-            print(i, child.text)
+        for i, child in enumerate(self.children[0].children[2].children):
             if (i % 2 == 0):
-                print('required')
                 contents.append(child.text)
-        print(contents)
-        print(contents[::2])
-        print(contents[1:][::2])
-        dict = {contents[1:][::2][i]: contents[::2][i] for i in range(len(contents[1:][::2]))}
-        print(dict)
+
+        if all([i != "" for i in contents]):
+
+            miRNANames = contents[1:][::2]
+            namesCheckSet = set(miRNANames)
+
+            miRNASequences = contents[::2]
+            seqCheckSet = set(miRNASequences)
+
+            if (len(seqCheckSet) == len(miRNASequences) and len(namesCheckSet) == len(miRNANames)):
+
+                checkList = [thsGen.checkCorrectCharacters(i) for i in miRNASequences]
+
+                if all(checkList):
+
+                    if all(len(i) > 14 for i in miRNASequences):
+                        dict = {miRNANames[i]: miRNASequences[i] for i in range(len(contents[1:][::2]))}
+                        thsGen.miRNADict = dict
+                        self.ids.promptLabel.text = "computing..."
+                        thsGen.start()
+                    else:
+                        self.ids.promptLabel.text = "sorry, our software doesn't support miRNA strands shorter than 15nt"
+                else:
+                    self.ids.promptLabel.text = "wrong input"
+            else:
+                self.ids.promptLabel.text = "multipule miRNA strands cannot share the same name or sequence"
+        else:
+            self.ids.promptLabel.text = "please fill in all of the boxes"
 
 
 
 
-class MyApp(App):
+
+class thsWindow(Screen):
+    pass
+
+class triggerWindow(Screen):
+    pass
+
+class complexWindow(Screen):
+    pass
+
+class ToeholdSwitchGenerator(App):
 
     def build(self):
-        return mainPage()
+        
+        Window.clearcolor = (10/225, 22/255, 40/255, 1)
+        sm = ScreenManager()
+        sm.add_widget(mainWindow())
+        sm.add_widget(thsWindow())
+        sm.add_widget(triggerWindow())
+        sm.add_widget(complexWindow())
+
+
+
+        return sm
 
 
 if __name__ == '__main__':
-    MyApp().run()
+    ToeholdSwitchGenerator().run()
