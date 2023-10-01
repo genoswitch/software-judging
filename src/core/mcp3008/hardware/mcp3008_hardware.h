23fdbb4d253904b3c4ddd4b77f4e9f9382bd5577
James Cahill <jcahill773@gmail.com>
2023-10-01 17:48:38 +0100

    Fix function header name

--- src/core/mcp3008/hardware/mcp3008_hardware.h
+++ src/core/mcp3008/hardware/mcp3008_hardware.h
@@ -7,6 +7,6 @@
 #include "../mcp3008.h"
 
 spi_dual_inst mcp3008_init_hardware(spi_inst_t *spi, uint baudrate, spi_pinout_t *pinout);
-uint16_t mcp3008_internal_do_adc(spi_dual_inst *inst, uint8_t channel, bool differential);
+uint16_t mcp3008_read_hardware(spi_dual_inst *inst, uint8_t channel, bool differential);
 
 #endif /* HARDWARE_MCP3008_HARDWARE_H_ */
